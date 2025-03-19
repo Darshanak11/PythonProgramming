7e94c48c7ed61fa17242bba2720f49048eb5d1fc
Darshana K <darshana884@gmail>
2025-03-19 14:15:57 +0530

    Added files

---
+++ Lamda.py
@@ -0,0 +1,13 @@
+def square(x):
+    return x ** 2
+
+print(square(10))
+
+def add(a,b):
+    return a + b
+
+print(add(3,4))
+
+number=[1,2,3,4,5]
+squared_numbers = [x** 2 for x in number]
+print(squared_numbers)
